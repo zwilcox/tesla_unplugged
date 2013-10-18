3e8df87779be3b0cc743b48374e1d969ed830dd3
rePharaoh <ari.ferro@gmail.com>
2013-10-18 12:33:38 -0600

    Created light sensor library using bitbang i2c
    Modified the existing ADJD-S311 library to use a software I2C library I
    found. This allows multiple I2C busses to be initialized, and also for
    multiple light sensors to be used simultaneously.

--- ADJD-S311(Arduino)[V2]/adjd_s311/adjd_s311.ino
+++ ADJD-S311(Arduino)[V2]/adjd_s311/adjd_s311.ino
@@ -1,33 +1,59 @@
-#include <ADJDS311.h>
-#include <Wire.h>
+#include <SoftADJDS311.h>
+#include <SoftI2C.h>
 
-#define SENSORLED 2 //LED on the ADJDS-311
-ADJDS311 color_sensor(SENSORLED);
+#define SENSORLED1 23 
+#define SENSORLED2 29 
+#define SDA1 24
+#define SCL1 25
+#define SDA2 26
+#define SCL2 27
+SoftADJDS311 color_sensor1(SENSORLED1,SDA1,SCL1);
+SoftADJDS311 color_sensor2(SENSORLED2,SDA2,SCL2);
 
 
 void setup(){
   Serial.begin(9600);
-  color_sensor.init();
+ 
+  color_sensor1.init();  
+  color_sensor1.ledOn(); //turn LED on
+ 
+  color_sensor2.init();  
+  color_sensor2.ledOn(); //turn LED on
+ 
+  Serial.println("Hold a wite peice of paper above sensor then press any key to calibrate...\n");
+  while(!Serial.available());
   
-  color_sensor.ledOn(); //turn LED on
   //Calibrate white 
   //Need to hold white card in front (1-3mm) of it to calibrate from
-  //color_sensor.calibrate();
-  color_sensor.ledOff();
+  color_sensor1.calibrate();  
+  color_sensor1.ledOff();
+  
+  color_sensor2.calibrate();  
+  color_sensor2.ledOff();
 
 }
 
 void loop(){
-  RGBC color = color_sensor.read(); //read the color
+  RGBC color1 = color_sensor1.read(); //read the color
+  RGBC color2 = color_sensor2.read(); //read the color
   
-  Serial.print(color.red);
+  Serial.print((color1.red/1024.0) * 255);
   Serial.print(" | ");
-  Serial.print(color.green);
+  Serial.print((color1.green/1024.0) * 255);
   Serial.print(" | ");
-  Serial.print(color.blue);
+  Serial.print((color1.blue/1024.0) * 255);
   Serial.print(" | ");
-  Serial.println(color.clear);
+  Serial.println((color1.clear/1024.0) * 255);
   
+  Serial.print((color2.red/1024.0) * 255);
+  Serial.print(" | ");
+  Serial.print((color2.green/1024.0) * 255);
+  Serial.print(" | ");
+  Serial.print((color2.blue/1024.0) * 255);
+  Serial.print(" | ");
+  Serial.println((color2.clear/1024.0) * 255);
+  
+  Serial.println();
   delay(500); //just here to slow down the serial outputs  
 }
 
